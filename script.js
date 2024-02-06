981e3ebe91c5b12add9b7f85965c99087f8c10b5
liviniapoo <s10255969@connect.np.edu.sg>
2024-02-06 16:05:52 +0800

    media query for SL page done

--- script.js
+++ script.js
@@ -14,19 +14,23 @@ NavItems.style.maxHeight="0vw";
 /*JS -  Accnt Toggle*/
 var SignupForm=document.getElementById("SignupForm");
 var LoginForm=document.getElementById("LoginForm");
-var indicator=document.getElementById("indicator");
 
     function signup(){
-        SignupForm.style.transform="translateX(0px)"
-        LoginForm.style.transform="translateX(0px)"
-        indicator.style.transform="translateX(15.5vw)"
+        SignupForm.style.transform="translateX(0px)";
+        LoginForm.style.transform="translateX(0px)";
     }
 
     function login(){
-        SignupForm.style.transform="translateX(50vw)"
-        LoginForm.style.transform="translateX(50vw)"
-        indicator.style.transform="translateX(0px)"
+        var screenSize=window.innerWidth;
+        if (screenSize<=800){
+            SignupForm.style.transform="translateX(77vw)";
+            LoginForm.style.transform="translateX(77vw)";
+        } else{
+            SignupForm.style.transform="translateX(50vw)";
+            LoginForm.style.transform="translateX(50vw)";
+        }
     }
+    
 
 /*JS - Accnt Creation*/
 document.addEventListener("DOMContentLoaded", function(){
